5be936ba190749e53a5c0ea6a42ca593e3364eb1
GrinIv <svetili@bk.ru>
2023-07-07 15:46:04 +0300

    seminar1 concatenation

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// int a = 25;
+
+// double b = 36.6;
+
+// string c = "Hello";
+
+// bool d = true;
+
+int number = 5;
+Console.WriteLine("my number" + number);
+
+
 Console.WriteLine("Hello, World!");
