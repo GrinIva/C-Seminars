8a8c0795c3d3a1e13637a0faf7ff1325a4baa8a9
GrinIv <svetili@bk.ru>
2023-07-07 17:00:25 +0300

    quad programm

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -20,7 +20,22 @@
 //      ВВод данных
 
 //Запрос данных от пользователя
-Console.Write("Input a number: ");
-int num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
 
 
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = num2 * num2;
+if(num1 == quad2)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("NO");
+}
