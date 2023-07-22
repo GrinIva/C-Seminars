b33ea19a2ad2b4cd3c63abcdb02f5d2658d4369e
GrinIv <svetili@bk.ru>
2023-07-22 21:13:26 +0300

    seminar5

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//напишите программу которая принимает на вход число А и выдает сумму от 1до А
+/*
+
+
+int Sum(int number)
+{
+    int count =0;
+    int sum = 0;
+    while (count < number)
+    {
+        sum += count;
+        count++;
+    }
+    return sum;
+
+}
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+*/
+//
+/*
+int Count(int number)
+{
+    int count = 0;
+    while (number / 10 > 0)
+    {
+        number = number / 10;
+        count ++;
+    }
+    return count;
+}
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("В вашем числе " + Count);
+
+
+// 2
+
+*/
+
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for(int i = 0; i< size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ")
+
+    Consol.WriteLine();
+}
---
+++ seminar5/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+// Задайте массив из 12 элементов и найдите сумму отрицательных элементов
+
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for(int i = 0; i< size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ")
+
+    Consol.WriteLine();
+}
+
+Console.WriteLine("Введите длинну массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение минимального элемента массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение максимального элемента массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myarray = CreateRandomArray(length, min, max);
+ShowArray(myarray);
